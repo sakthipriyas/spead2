533a6c98ded586286add5757e78a270428e122bc
Bruce Merry <bmerry@ska.ac.za>
2018-07-29 19:03:04 +0200

    Work around rvm cd nonsense

--- bisect-test.sh
+++ bisect-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-cd ..
+builtin cd ..
 python -m pip install -v -e .
 timeout 5 python -c "import hangup; hangup.foo('')"
