2fe3b618e0436a7e9b21e71ee1c74a7e49bda134
Bruce Merry <bmerry@ska.ac.za>
2018-07-29 19:39:22 +0200

    Swap meanings of good and bad
    bisect assumes you're looking for a regression, rather than when
    something got fixed.

--- bisect-test.sh
+++ bisect-test.sh
@@ -2,4 +2,8 @@
 
 builtin cd ..
 python -m pip install -v -e .
-gtimeout 5 python -c "import hangup; hangup.foo('')"
+if gtimeout 5 python -c "import hangup; hangup.foo('')"; then
+    exit 1
+else
+    exit 0
+fi
